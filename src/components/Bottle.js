81b653f4778b1e3d4412d8a9831b1e7242546bd3
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-11 10:10:51 -0700

    add code fro bottle.js

---
+++ src/components/Bottle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Bottle(props){
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenBottleClicked(props.id)}>
+        <p>{props.name}</p>
+        <p>{props.type}</p>
+        <p>{props.price}</p>
+        <p>{props.origin}</p>
+        <p>{props.tastingNotes}</p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Ticket.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.string,
+  origin: PropTypes.string,
+  tastingNotes: PropTypes.string,
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
+};
+
+export default Ticket;
