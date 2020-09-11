3d171b155b0306c140ac64a287b087c1217d1eb3
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-11 09:54:47 -0700

    add diagram for app design

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import BottleControl from "./BottleControl";
+
+function App(){
+  return (
+    <React.Fragment>
+      <NavBar />
+      <BottleControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
